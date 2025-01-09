cdc5910d9916465bdd7be43e84068515eacb6c90
Astrallist <132389598+Astrallist@users.noreply.github.com>
2025-01-09 18:28:56 +0300

    Update script.js

--- script.js
+++ script.js
@@ -171,6 +171,15 @@ function createTaskParagraph(task) {
                 alignment: docx.AlignmentType.LEFT,
             }),
 
+                new docx.Paragraph({
+                alignment: docx.AlignmentType.LEFT,
+                children: [
+                    new docx.TextRun({
+                        text: 'Посредник: _________________',
+                    })
+                ]
+            }),
+
             new docx.Paragraph({
                 text: task.squad,
                 alignment: docx.AlignmentType.LEFT,
@@ -297,6 +306,18 @@ function createTaskParagraph(task) {
                     })
                 ]
             }),
+
+            new docx.Paragraph({
+                alignment: docx.AlignmentType.LEFT,
+                children: [
+                    new docx.TextRun({
+                        text: 'Посредник: _________________',
+                        size: 20,
+                    })
+                ]
+            }),
+
+            
             new docx.Paragraph({
                 alignment: docx.AlignmentType.LEFT,
                 children: [
